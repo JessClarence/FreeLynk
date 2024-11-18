f4847fb361a53160408c490bb6a3dd38fc00023f
Jess Clarence <67852370+JessClarence@users.noreply.github.com>
2024-11-18 10:59:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    assetsDir: 'assets',  // Specify where to place assets like images, fonts, etc.
+  },
+  publicDir: 'resources/img',  // Include your images directory here for static files
 });
